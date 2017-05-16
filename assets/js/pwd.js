a1c7cadb59fb3484a79765881de689b7962e637e
Jonathan Leibiusky @xetorthio <ionathan@gmail.com>
2017-05-16 12:18:51 -0300

    Lots of changes

--- assets/js/pwd.js
+++ assets/js/pwd.js
@@ -1,20 +1,52 @@
-$(document).ready(function() {
-    var chunks = window.location.pathname.split('/');
-    var path;
-    console.log(chunks);
-    if (chunks[1] == '_') {
-        // This is an official repo
-        path = '/' + chunks[2];
-    } else {
-        // This is a repo of an organization
-        path = '/' + chunks[2] + '/' + chunks[3];
+addDialog();
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        setCurrentRepo(request.repo);
+        if (!$('#try-in-pwd').length) {
+            $('section.secondary-top-bar-section ul').append('<li><a id="try-in-pwd" data-toggle="modal" data-target="#try-in-pwd-modal"><img src="' + chrome.extension.getURL('assets/images/button.png') + '" /></a></li>');
+        }
     }
+);
 
-    $.get('https://api.github.com/repos/play-with-docker/stacks/contents' + path)
-        .done(function(data) {
-        })
-        .fail(function() {
-        });
+function setCurrentRepo(repo) {
+    var appElement = document.querySelector('[ng-app=PWD]');
+    var $scope = angular.element(appElement).scope();
+    if ($scope) {
+        $scope.changeRepo(repo);
+    }
+}
 
-    // $('section.secondary-top-bar-section ul').append('<li><a><img src="' + chrome.extension.getURL('assets/images/button.png') + '" /></a></li>');
-});
+function addDialog() {
+    var container = $('body').append('<div id="pwd-dialog"></div>');
+    $('#pwd-dialog').load(chrome.extension.getURL('assets/html/dialog.html'));
+}
+
+angular.module('PWD', [])
+    .controller('TryController', ['$scope', function($scope) {
+        $scope.currentRepo = null;
+        $scope.tags = [];
+
+        var pwd = this;
+
+        $scope.changeRepo = function(repo) {
+            if (repo != $scope.currentRepo) {
+                $scope.currentRepo = repo;
+                $.get('https://api.github.com/repos/play-with-docker/stacks/contents' + repo)
+                    .done(function(data) {
+                        $scope.$apply(function() {
+                            $scope.tags = data;
+                        });
+                    })
+                    .fail(function(resp) {
+                        if (resp.status == 404) {
+                            $scope.$apply(function() {
+                                $scope.tags = [];
+                            });
+                        } else {
+                            console.error('Could not retrieve stack tags.');
+                        }
+                    });
+            }
+        }
+    }]);
+
